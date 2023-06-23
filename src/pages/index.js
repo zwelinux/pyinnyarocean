e23d524589cc6d056a9aa2adeecb72835bf7b8c2
zwelinux <zwelinux@gmail.com>
2023-06-23 20:56:37 +0630

    index page update

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,8 @@ const IndexPage = () => {
       <Navbar />
       <Main routeName="HOME PAGE ROUTE">
         <div class="main-container">
-            <img src="https://images.unsplash.com/photo-1518467166778-b88f373ffec7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2531&q=80" width="100%" alt="home" />
-            <p class="main-container-text">Some Codes are Better Be Deployed!</p>
+            <img src="https://images.unsplash.com/photo-1467173195972-87d54818ee82?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2072&q=80" width="100%" alt="home" />
+            <p class="main-container-text">WELCOME TO PYIN NYAR OCEAN</p>
             <br />
             <a href="#" class="button">JOIN</a>
         </div>
